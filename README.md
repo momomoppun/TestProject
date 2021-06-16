3dd7a41f317fe01e36f33bbef1410360fc8ede3f
60252 <60252@ITS00236.mjsystem.com>
2021-06-16 16:24:10 +0900

    arata

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TestProject
 test project!!
+aaaaa
--- test001/src/main/java/test001/HelloGit.java
+++ test001/src/main/java/test001/HelloGit.java
@@ -5,7 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello git");
-		
+		System.out.println("test");
 	}
 
 }
